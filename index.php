8eeea1199281a3aa99e11a80731c75c0b14001d0
ronielrpl1 <ronielpereiraleite@gmail.com>
2022-10-31 09:16:41 -0300

    testando arquivos

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 <head>
   <link rel="stylesheet" href="_css/estilo.css"/>
   <meta charset="UTF-8"/>
-  <title>Curso de PHP - CursoemVideo.com</title>
+  <title>Curso de PHP - Roniel Pereira</title>
 </head>
 <body>
 <div>
-    <?php
-        echo "teste de PHP";
+	<?php
+        echo"teste de php funciona";
     ?>
 </div>
 </body>
